7d54608eaea2ccdebd722fd21363598c35de4b76
Celeste Carignano <ccarignano@gmail.com>
2022-02-15 21:32:48 -0300

    add console.log

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -3,6 +3,7 @@ var md5 = require('md5');
 
 async function getUser(user, password) {
     try {
+        console.log('datos de conexion: '+process.env.MySQL_HOST+' - '+process.env.MySQL_USER+' - '+process.env.MySQL_PASSWORD+' - '+process.env.MySQL_DB_NAME)
         var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
         var rows = await pool.query(query, [user, md5(password)]);
         return rows[0];
